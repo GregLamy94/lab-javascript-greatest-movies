523bfe2e47be6ef6c3fdc644e45bc2e8b3e878e9
Gregoire Lamy <g.aderlamy@gmail.com>
2019-10-01 21:43:40 +0200

    finaltry

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -29,14 +29,59 @@ function dramaMoviesRate (tabMovies) {
 }
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
+function calculateDuration (movie) {
+    var hmin=Number(movie.duration.split(" ")[0].split("h")[0])*60 ; 
+    var min=Number(movie.duration.split(" ")[1].split("m")[0]) ; 
+    return hmin+min ; 
+}
 
+function compareMovies (first, second) {
+   if (calculateDuration(first) < calculateDuration(second)) {
+     return -1  ; 
+   } else if (calculateDuration(first) > calculateDuration(second)) {
+    return 1 ; 
+   } else if (first.title < second.title) {
+       return -1 ; 
+   } else if (first.title > second.title) {
+   return 1 ; 
+   }
+   else return 0 ; 
+} 
 
+function orderByDuration(tabMovies) {
+    return tabMovies.splice().sort(compareMovies); 
+}
 
 
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 
+function howManyMovies(tabMovies) {
+    return tabMovies.filter(function(movie) {
+         return movie.genre.includes("Drama") && movie.director==="Steven Spielberg" ; 
+}).length} 
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(tabMovies){
+    var tableauTrie = tabMovies.splice().sort(function(a,b) {
+        if (first.title < second.title) {
+            return -1 ; 
+        } else if (first.title > second.title) {
+        return 1 ;  
+        } else return 0 ; 
+    });
+    if (tableauTrie.length <= 20 ) {
+        return tableauTrie.map(function(movie){
+            return movie.title ; 
+        }); 
+    }
+    else if (tableauTrie.length > 20 ) {
+        return tableauTrie.map(function(movie){
+            return movie.title ; 
+        }).splice(0,20); 
+    }
+}
+
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
