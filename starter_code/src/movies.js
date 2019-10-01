eefca72866f6eee6491357966a6d789d4eacf404
Gregoire Lamy <g.aderlamy@gmail.com>
2019-10-01 20:32:54 +0200

    itération2

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,22 +2,35 @@
 
 // import {movies} from `/data.js` ; 
 
+
+// Iteration 1: All rates average - Get the average of all rates with 2 decimals 
+
+
 function ratesAverage (tabMovies) {
     var totalNotes = tabMovies.reduce(function(sum, note){
         return sum + note.rate;
       },0); 
-      return totalNotes/tabMovies.length ; 
+      return Math.round((totalNotes/tabMovies.length)*100)/100 ; 
     }
-
-
-// Iteration 1: All rates average - Get the average of all rates with 2 decimals 
 
 
  
 // Iteration 2: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRate (tabMovies) {
+    var dramaMovies = tabMovies.filter(function(movie) {
+        return movie.genre.includes("Drama"); 
+    });
+    if (!dramaMovies.length) {
+        return 0;
+    }
+    return ratesAverage(dramaMovies) ; 
+
+}
 
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
+
+
 
 
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
